d7760f2ffb83e70fbed8e725f2e2dcf021065e6b
musharaf Faqi <maamaanka55@gmail.com>
2023-04-25 11:14:02 +0300

    Success

--- main/views.py
+++ main/views.py
@@ -7,40 +7,8 @@ def index(request):
     totalCompany = Campany.objects.count()
     context = {
         'company':totalCompany
-        }
-    
+        }    
     return render(request, 'index.html',context)
-
-
-
-
-
-def network(request):
-
-    return render(request,'pages/network.html')
-
-def host(request):
-
-    return render(request,'pages/host.html')
-
-
-
-
-def ports(request):
-    return render(request,'pages/port.html')
-
-def compare(request):
-    return render(request,'pages/compare.html')
-
-
-
-def scan_case(request):
-    return render(request,'pages/scan_case.html')
-
-
-def Reports2(request):
-    return render(request,'pages/Reports2.html')
-
 
 
 
