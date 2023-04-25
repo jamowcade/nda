ced719afde7e27b65adb946a4bf0656d5525ba57
musharaf Faqi <maamaanka55@gmail.com>
2023-04-25 14:07:32 +0300

    Success

--- main/code/Network.py
+++ main/code/Network.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from main.models import Campany
+from main.models import Campany,Network
 
 # Create your views here.
 
-def network(request):
+def network(request,id):
 
-    return render(request,'pages/network.html')
+    companyList = Campany.objects.all()
+    networkList = Network.objects.all()
+    singleNetwork = Network.objects.filter(compony_info = id).all()
+    context = {
+        'companyData': companyList,
+        'networkData': networkList,
+        'singleNetwork': singleNetwork
+    }
+
+    # for n in networkList:
+    #     print(n.compony_info)
+    # print(singleNetwork)
+
+    return render(request,'pages/network.html',context)
