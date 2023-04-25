c640346408d3f3008d2ac541fec32f4ab575c6a4
musharaf Faqi <maamaanka55@gmail.com>
2023-04-25 14:07:25 +0300

    Success

--- main/views.py
+++ main/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from main.models import Campany
+from main.models import Campany,Network
 
 # Create your views here.
 
 def index(request):
     totalCompany = Campany.objects.count()
+    totalNetwork = Network.objects.count()
     context = {
-        'company':totalCompany
+        'company':totalCompany,
+        'network':totalNetwork
         }    
     return render(request, 'index.html',context)
 
