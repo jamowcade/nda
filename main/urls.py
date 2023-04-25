4f599d20c1eed03e40cf0ca593c7039072c093f6
musharaf Faqi <maamaanka55@gmail.com>
2023-04-25 10:42:36 +0300

    Success

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from main import views
-from .code import user
+from .code import user,company
 
 
 urlpatterns = [
@@ -9,12 +9,12 @@ urlpatterns = [
     path('login/', user.login, name='login'),
     path('register/', user.register, name='register'),
     path('forgot/', user.forgot, name='forgot'),
-    path('staffs/', views.staffs, name='staffs'),
+    path('staffs/', user.staffs, name='staffs'),
 
 
     #region Environment
 
-    path('company/', views.company, name='company'), 
+    path('company/', company.company, name='company'), 
     path('network/', views.network, name='network'),
     path('host/', views.host, name='host'),
 
