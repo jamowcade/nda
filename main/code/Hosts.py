c65796eb4125581e243592cc18eb6adaf7504cb3
jamowcade <ayanlestyle@gmail.com>
2023-05-22 16:28:46 +0300

    ok

--- main/code/Hosts.py
+++ main/code/Hosts.py
@@ -70,7 +70,6 @@ def addHosts(request):
                 
                 is_host = Host.objects.filter(hostname=hostname, host_date=scan_date, network=network).all() # check if host exists with the given hostname and scan date.
                 
-<<<<<<< HEAD
                 # check if host already registered in the current scan case
                 if is_host:
                     ErrorLog.objects.create(
@@ -113,57 +112,6 @@ def addHosts(request):
         else:
             return JsonResponse({'success': False, 'error': 'Invalid request method'})
   
-=======
-                # retrieve data from json file and save to model
-                for host in range(len(data)-1):
-                    hostname = data[host]['ip']
-                    status =   data[host]['state']
-                    network = host_network
-                    
-                    is_host = Host.objects.filter(hostname=hostname, host_date=scan_date).all() # check if host exists with the given hostname and scan date.
-                    
-                    # check if host already registered in the current scan case
-                    if is_host:
-                        ErrorLog.objects.create(
-                        user=request.user,
-                        message=f'This file already uploaded for network - ({file_network}) at {scan_date}. Please upload another file'
-                        )
-                        return JsonResponse({'success': False, 'error': 
-                        f'sorry! this file already uploaded for network - ({file_network}) at {scan_date}. Please upload another file or change'})
-                        continue
-                    else:
-                        new_host = Host(hostname=hostname, status = status, network=network, host_date=scan_date)
-                        new_host.save()
-                        for port in range(len(data[host]['ports'])):
-                            portid= data[host]['ports'][port]['portid']
-                            protocol = data[host]['ports'][port]['protocol']
-                            state = data[host]['ports'][port]['state']
-                            # try:
-                            #     for key, value in data[host]['ports'][port]['service']:
-                            #         print(key, value)
-                            # except:
-                            #     return JsonResponse("error getttin services", safe=False)
-                            service_name =  data[host]['ports'][port]['service']['name']
-                            service_version =  data[host]['ports'][port]['service']['version']
-                            service_ostype =  data[host]['ports'][port]['service']['ostype']
-                            service_devicetype =  data[host]['ports'][port]['service']['devicetype']
-                            service_product =  data[host]['ports'][port]['service']['product']
-                            service = f"name: {service_name}, version: {service_version}, Product: {service_product}, Ostype: {service_ostype},deviceType:{service_devicetype}"
-                            # for service in range(1):
-                            #     service_name
-                            reason = data[host]['ports'][port]['state']
-                            port = Port(port=portid, state = state, protocol=protocol, host=new_host, service=service, reason=reason)
-                            port.save()
-            
-                return JsonResponse({'success': True, "message": f"All hosts Uploaded to network {network.compony_info.owner} - {file_network}", })
-                UserLog.objects.create(
-                        user=request.user,
-                        message = f'{request.user} uploaded ({file_network}) file for scan case ({scan_date})'
-                        )
-            else:
-                return JsonResponse({'success': False, 'error': 'Invalid request method'})
-    
->>>>>>> f3a2ea9932d6a8baeda0ffbced1088facc8569bd
     except Exception as e:
                     tb = traceback.format_exc()
                     message = f"An error occurred: while uploading file {e}"
