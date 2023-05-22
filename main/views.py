8128ef4f543d56f68309ff9e8cd372a46bef2fa0
musharaf Faqi <maamaanka55@gmail.com>
2023-05-22 11:01:32 +0300

    Finished By Chart Data

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models.functions import ExtractMonth
 from main.models import Campany,Network,Host,Port,ScanCase,UserLog
 from django.contrib.auth.decorators import login_required, permission_required
 from django.db.models import Count
@@ -7,6 +8,7 @@ from django.db.models import Count
 @login_required(login_url='login')
 def index(request):
     eachPort = []
+    monthList = []
 
     # Count Each Table Of Database
     totalCompany = Campany.objects.count()
@@ -14,9 +16,17 @@ def index(request):
     totalHost = Host.objects.count()
     totalPorts = Port.objects.count()
     
-    # ppp = Port.objects.annotate(count=Count('port')).order_by('-count')[:5]
-    # for p in ppp:
-    #     print(p.port)
+    dataMonth = Host.objects.annotate(month=ExtractMonth('host_date')).order_by('month')
+    distinct_dates = Host.objects.dates('host_date', 'month')
+    for d in distinct_dates:
+        month_name = d.strftime('%B')
+        print(month_name)
+        print("Month",dataMonth.count())
+        monthList.append({
+            'months':month_name,
+            'hosts':dataMonth.count(),
+        })
+
 
     each_port_value = Port.objects.values('port').annotate(count=Count('port')).order_by('-count')[:5][::1]
 
@@ -47,7 +57,7 @@ def index(request):
               'count':each['count'],
               'percentage':each['percentage'],
          })
-    print(eachPort)
+    # print(eachPort)
    
     context = {
         'company':totalCompany,
@@ -63,6 +73,7 @@ def index(request):
         'filterport':filterPorts,
         'top_scan':top_scan,
         'user_logs':user_logs,
+        'dataChart':monthList
         }    
     return render(request, 'index.html',context)
 
