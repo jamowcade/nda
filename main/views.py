66edffaf33d32cae7c3a7921219a5d97a4cde40c
musharaf Faqi <maamaanka55@gmail.com>
2023-05-23 18:14:40 +0300

    Changed Some Files

--- main/views.py
+++ main/views.py
@@ -9,6 +9,9 @@ from django.db.models import Count
 def index(request):
     eachPort = []
     monthList = []
+    topCompanyList = []
+    topNetworkList = []
+    topHost = []
 
     # Count Each Table Of Database
     totalCompany = Campany.objects.count()
@@ -16,6 +19,8 @@ def index(request):
     totalHost = Host.objects.count()
     totalPorts = Port.objects.count()
     
+
+    # Change Date to Month Name and Adde Them To a List
     dataMonth = Host.objects.annotate(month=ExtractMonth('host_date')).order_by('month')
     distinct_dates = Host.objects.dates('host_date', 'month')
     for d in distinct_dates:
@@ -28,7 +33,27 @@ def index(request):
         })
 
 
-    each_port_value = Port.objects.values('port').annotate(count=Count('port')).order_by('-count')[:5][::1]
+
+    #Get Top Company In Networks and them to the list
+    same_company_count = Network.objects.values('compony_info').annotate(count=Count('compony_info_id')).filter(count__gt=1).order_by('-count')[:5][::1]
+    sum_com = 0
+    for same_company in same_company_count:
+        comp_id = same_company['compony_info']
+        count_company = same_company['count']
+        sum_com += count_company
+        get_in_company = Campany.objects.filter(id=comp_id).all()
+        same_company['percentage'] = round(count_company / sum_com * 100)
+        for singlecom in get_in_company:
+
+            topCompanyList.append({
+                'company': singlecom.owner,
+                'count': count_company,
+                'percentage': same_company['percentage'],
+                
+
+            })
+    # print(sum_com)
+
 
     # Geting Ports State By Filtering "Open","Closed" & "Filtered"
 
@@ -36,7 +61,6 @@ def index(request):
     closePorts = Port.objects.filter(state="closed").count()
     filterPorts = Port.objects.filter(state="filtered").count()
     
-
     openstate = getOpenPorts(totalPorts,openPorts)
     closestate = getClosedPort(totalPorts,closePorts)
     filterstate = getfilterPorts(totalPorts,filterPorts)
@@ -47,7 +71,9 @@ def index(request):
     user_logs = UserLog.objects.all().order_by('-created_at')[:5][::1]
 
 
+    # Get top 5 hight Ports and add them to List
     
+    each_port_value = Port.objects.values('port').annotate(count=Count('port')).order_by('-count')[:10][::1]
   
     for each in each_port_value:
          each['percentage'] = round(each['count'] / totalPorts * 100, 2)
@@ -57,7 +83,68 @@ def index(request):
               'count':each['count'],
               'percentage':each['percentage'],
          })
-    # print(eachPort)
+    
+
+    # Get Top Hight Host in Networks
+    same_network = Host.objects.values('network_id').annotate(count=Count('network_id')).filter(count__gt=1).order_by('-count')[:5][::1]
+    
+    for single_network_id in same_network:
+        host_id = single_network_id['network_id']
+        count_host = single_network_id['count']
+        get_each_host_id = Host.objects.filter(id=host_id).all()
+        for each_host in get_each_host_id:
+            print(each_host)
+            network = each_host.network.network 
+            company = each_host.network.compony_info.owner
+            topNetworkList.append({
+                'network': network,
+                'company': company,
+                'count': count_host,
+
+            })
+
+    print("------>>>> ",topNetworkList)
+    
+
+    
+    # Get The Highest Ports in Host and Adding them to the list
+
+    same_host = Port.objects.values('host_id').annotate(count=Count('host_id')).filter(count__gt=1).order_by('-count')[:5][::1]
+    sum_host = 0
+    for single_host_id in same_host:
+        port_id = single_host_id['host_id']
+        count_port = single_host_id['count']
+        sum_host+=count_port
+        single_host_id['percentage'] = round(count_port / sum_host * 100)
+        get_each_port_id = Port.objects.filter(id=port_id).all()
+        for each_port in get_each_port_id:
+            host = each_port.host.hostname
+            network = each_port.host.network.network
+            company = each_port.host.network.compony_info.owner
+            topHost.append({
+                'host': host,
+                'network': network,
+                'company': company,
+                'count': count_port,
+                'percentage': single_host_id['percentage'],
+
+
+            })
+    # print(topHost)
+            # print(each_port.host.network.compony_info.owner)
+
+
+
+    # print(same_host)
+
+
+
+        # print(single_host_id)
+
+    
+
+
+
    
     context = {
         'company':totalCompany,
@@ -73,7 +160,9 @@ def index(request):
         'filterport':filterPorts,
         'top_scan':top_scan,
         'user_logs':user_logs,
-        'dataChart':monthList
+        'dataChart':monthList,
+        'topCompany':topCompanyList,
+        'topHost':topHost
         }    
     return render(request, 'index.html',context)
 
