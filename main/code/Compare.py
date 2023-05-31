3149148c928c40208ba64584bd875b964ade0b66
Eghamza <ciidcade143@gmail.com>
2023-05-31 16:27:50 +0300

    Finished Compare File

--- main/code/Compare.py
+++ main/code/Compare.py
@@ -10,7 +10,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.paginator import Paginator
 # Create your views here.
 
-
+list_ids = []
 @permission_required('main.compare_scancase', raise_exception=True, login_url=None)
 def compare(request):
     name = request.GET.get('name')
@@ -33,32 +33,29 @@ def get_campany_name(request):
 
 @login_required(login_url='login')
 def compare_by_date(request):
-    compare_date = request.GET.get('FILTERED_DATE')
-    compare_date1 = request.GET.get('FILTERED_DATE1')
-    compare_date2 = request.GET.get('FILTERED_DATE2')
-   
+    
+    scan_case_id1 = request.GET.get('FILTERED_DATE1')
+    scan_case_id2 = request.GET.get('FILTERED_DATE2')
+    print("------->",scan_case_id1, scan_case_id2)
     compare_network = request.GET.get('network')
 
 
-    scan_date_1 = datetime.strptime(compare_date1, "%Y-%m-%d").date()
-    scan_date_2 = datetime.strptime(compare_date2, "%Y-%m-%d").date()
+    
 
     network = request.GET.get('network')   
     company = request.GET.get('company')
 
     network_id  = Network.objects.get(id=network)
-    date_object = datetime.strptime(compare_date1, "%Y-%m-%d").date()
-    date_object2 = datetime.strptime(compare_date2, "%Y-%m-%d").date()
-
-    scan_case_one = ScanCase.objects.get(scan_date=date_object)
-    scan_case_two = ScanCase.objects.get(scan_date=date_object2)
    
-    all_host1 = Host.objects.filter(host_date=date_object,network= network_id).all() #get all hosts by network and host_date1
-    all_host2 = Host.objects.filter(host_date=date_object2,network= network_id).all() #get all hosts by network and host_date2
-    # ports = all_host1.ports.all()
-    # print("host 1",all_host1)
-    # print("host 2",all_host2)
+    scan_case_one = ScanCase.objects.get(id = scan_case_id1)
+    scan_case_two = ScanCase.objects.get(id = scan_case_id2)
     
+    list_ids.append(scan_case_id1)
+    list_ids.append(scan_case_id2)
+   
+    all_host1 = Host.objects.filter(scan_case_id = scan_case_id1,network= network_id).all() #get all hosts by network and host_date1
+    all_host2 = Host.objects.filter(scan_case_id = scan_case_id2,network= network_id).all() #get all hosts by network and host_date2
+   
    
 
     all_h2 =[]# new list holds all hosts with scan_date
@@ -87,31 +84,50 @@ def compare_by_date(request):
     
     comman= set(all_h1).intersection(all_h2) #get the comman elements 
     common_hosts = []
+    list_hosts1_ports = []
+    list_hosts2_ports = []
+    last_ports = []
+    # print(comman)
     for host in comman:
         host1 = scan_case_one.hosts.get(hostname=host,network= network_id)#get the host 
-        hosts1_ports = host1.ports.all().count()
+        hosts1_ports = host1.ports.all()
         host2 = scan_case_two.hosts.get(hostname=host,network= network_id)#get the host 
-        hosts2_ports = host2.ports.all().count()
-        print(host,hosts1_ports, hosts2_ports,"there no differnce in ports")
-        host1_services = 0
-        host2_services = 0
-        # for port1 in host1.ports.all():
-        #     host1_services = port1.services.all().count()
-        # for port2 in host2.ports.all():
-        #     host2_services = port2.services.all().count()
-        if hosts1_ports != hosts2_ports:
-            for port1 in host1.ports.all():
-                host1_services = port1.services.all().count()
-            for port2 in host2.ports.all():
-                 host2_services = port2.services.all().count()
-        if hosts1_ports != hosts2_ports:
+        hosts2_ports = host2.ports.all()
+
+        for port in hosts1_ports:
+            list_hosts1_ports.append((host1.hostname,port.port,port.state))
+
+        for port in hosts2_ports:
+            list_hosts2_ports.append((host2.hostname,port.port,port.state))
+
+        port_difference = set(list_hosts1_ports).symmetric_difference(set(list_hosts2_ports)) # get the difference
+        port_dff = list(port_difference)
+
+        if len(port_dff) > 0:
+            last_ports.append(host1.hostname)
+            # list_ids.append(host1.scan_case_id)
+            # list_ids.append(host2.scan_case_id)
+
+            # last_ports.append((host2.hostname, host2.scan_case_id))
+                
+    result = getALl(last_ports,network_id)
+    print(last_ports)
+    if result:
+        paginator = Paginator(result, 10)
+        page_number = request.GET.get('page')
+        pagePaginator= paginator.get_page(page_number)
             
-            print(host,"total ports host 1",hosts1_ports,"total ports host 2", hosts2_ports, "total services 1", host1_services, "total services 1", host2_services,"there differnce in ports")
+        data = {
+                'comman_ports':pagePaginator,
+                
+                'network':compare_network,
+            }
+        return render(request,'pages/show_cmpr.html',data)
+
 
     
-    
     if len(dff) != 0:
-        a =  getALl(dff)
+        a =  getALl(dff,network_id)
        
         paginator = Paginator(a, 10)
         page_number = request.GET.get('page')
@@ -119,8 +135,7 @@ def compare_by_date(request):
         
         data = {
             'records':pagePaginator,
-            'scan_date1':scan_date_1,
-            'scan_date2':scan_date_2,
+            
             'network':compare_network,
         }
         return render(request,'pages/show_cmpr.html',data)
@@ -142,7 +157,7 @@ def compare_by_date(request):
     
         return render(request,'pages/show_cmpr.html',data)
     else:
-        a =  getALl(dff)
+        a =  getALl(dff,network_id)
        
         paginator = Paginator(a, 10)
         page_number = request.GET.get('page')
@@ -150,8 +165,7 @@ def compare_by_date(request):
         
         data = {
             'records':pagePaginator,
-            'scan_date1':scan_date_1,
-            'scan_date2':scan_date_2,
+
             'network':compare_network,
         }
     
@@ -203,13 +217,16 @@ def get_Hosts(request):
        
         return JsonResponse(data)
 
-def getALl(all_dff):
+def getALl(all_dff,network):
 
     port_with_host = []
     for host in all_dff: #make enumerate all
+       ids = list_ids
+       
     #    print(host[1::2])
-       get_host_id = Host.objects.filter(hostname=host).all()
+       get_host_id = Host.objects.filter(hostname=host,scan_case_id__in = ids).all()
        for host_id in get_host_id:
+
         
         ports = host_id.ports.all()
         port_with_host.append({
